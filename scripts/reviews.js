afe8bb5c97f1b634c355af1ca42c5a16e71b898d
Dopeycylender39 <briggsjjacob@gmail.com>
2023-01-10 23:44:06 -0700

    Reviews section

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,50 +1,15 @@
-// reviews
-const testimonials = [{
-    image: "images/2616451236_56e5a21b0f_b.jpg",
-    name: "John Williker",
-    testimonial:
-      "BPA Auto Sales was there for me when my old car broke down. They hooked my up with a sweet new ride fast and at a reasonable price!",
-  },
-  {
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
 
-    image: "images/PIXNIO-2568248-3840x5760.jpg",
-    name: "Joey Droop",
-    testimonial:
-      "As a busy business man and father of 4, I don't have a lot of time. BPA helped me purchase a new car extremely quickly. I am very impressed with their customer service.",
-  },
-  {
-    image: "images/Testimonial_1.jpg",
-    name: "Malanie Milintoff",
-    testimonial:
-      "It's hard to find affordable cars these days. I am fortunate to have found BPA so that I can have a car that fits my budget!",
-  },
-  {
-    image: "images/winniethepooh_egmont.jpg",
-    name: "Anonymous",
-    testimonial:
-      "Hmm, well I must say I enjoy the hunny car I purchased. Talking about hunny, I could really go for a smackeral right about now...",
-  },
-];
-//Current Slide
-let i = 0;
-//Total Slides
-let j = testimonials.length;
-let testimonialContainer = document.getElementById("testimonial-container");
-let nextBtn = document.getElementById("next");
-let prevBtn = document.getElementById("prev");
-nextBtn.addEventListener("click", () => {
-  i = (j + i + 1) % j;
-  displayTestimonial();
-});
-prevBtn.addEventListener("click", () => {
-  i = (j + i - 1) % j;
-  displayTestimonial();
-});
-let displayTestimonial = () => {
-  testimonialContainer.innerHTML = `
-    <img src=${testimonials[i].image}>
-    <p>${testimonials[i].testimonial}</p>
-    <h3>${testimonials[i].name}</h3>
-  `;
-};
-window.onload = displayTestimonial;
+function next(){
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}
+
+function prev(){
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+}
+
